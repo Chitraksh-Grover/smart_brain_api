769d290b43565fdb0f04c311eb4a6d03bee45f65
Chitraksh Grover <chitrakshgrover@gmail.com>
2020-04-25 19:25:31 +0530

    database connections

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex')
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
 	client: 'pg',
 	connection: {
