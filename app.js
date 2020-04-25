34e3aaccc27a7d3f18a9c716069f95cba67b9bd3
Chitraksh Grover <chitrakshgrover@gmail.com>
2020-04-25 18:42:52 +0530

    updating database

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const knex = require('knex')
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
+		host: 'postgresql-reticulated-48566',
 		user: 'chitraksh',
 		password: 'Cg@96.40',
 		database: 'smartbrain'
